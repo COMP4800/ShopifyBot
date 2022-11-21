ba37322b8fcfe019c40a489931fbeb96d1de085b
JashanpreetSingh-dev <77453449+JashanpreetSingh-dev@users.noreply.github.com>
2022-11-21 09:30:00 -0800

    Add a scheduler

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from connection import wrapper
+from apscheduler.schedulers.background import BlockingScheduler
+scheduler = BlockingScheduler()
 
 
 def main():
-    wrapper()
+    scheduler.add_job(id='Scheduled task', func=wrapper(), day_of_week='mon-sun', hour=1)
+    scheduler.start()
+    # wrapper()
 
 
 if __name__ == '__main__':
